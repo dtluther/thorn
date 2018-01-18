31a52ab7b4989f7e92852f5953a7e51fdb0666d4
Dillon <dillontluther@gmail.com>
2018-01-18 09:34:16 -0800

    adding some comments for clarity

--- pretty_scrape.rb
+++ pretty_scrape.rb
@@ -1,5 +1,6 @@
 require_relative 'scraper'
 
+# Write to file so we can `pretty_generate` the JSON format in the terminal
 File.open("pretty_output.json", "w") do |f|
     f.write("#{JOINED}")
 end
--- scraper.rb
+++ scraper.rb
@@ -8,13 +8,14 @@ require 'HTTParty'
 require 'Pry'
 require 'json'
 
+# Load and parse the page
 page = HTTParty.get('https://www.wearethorn.org/resources-and-research')
-
 parsed_page = Nokogiri::HTML(page)
 
 links = []
 projects = {}
 
+# Locate the projects
 parsed_page.css('.standard_section')[4..-3].each do |project_section|
     project_section.css('.vc_column-inner').each do |project|
         title = project.css('h4')
@@ -26,6 +27,7 @@ parsed_page.css('.standard_section')[4..-3].each do |project_section|
     end
 end
 
+# Get the links that don't have '#' at the beginning of the `href` tag
 parsed_page.css('a').each do |link|
     next unless link["href"]
     unless link["href"][0] == "#"
@@ -35,8 +37,10 @@ parsed_page.css('a').each do |link|
     end
 end
 
+# Put everything in a JSON constant that can be accessed in another file if necessary
 JOINED = { links: links, projects: projects}.to_json
 
+# Only print JOINED to STDOUt if this file is directly run, not if required by `pretty_scrape.rb`
 if __FILE__ == $0
     puts JOINED
 end
